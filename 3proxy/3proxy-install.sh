4d3a309745d9be4a2fb3b9032f7c2034577009dd
weselow <598746@gmail.com>
2021-05-31 14:28:49 +0400

    Update 3proxy-install.sh

--- 3proxy/3proxy-install.sh
+++ 3proxy/3proxy-install.sh
@@ -8,12 +8,6 @@ echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 wget -O /opt/3proxy-0.9.3.x86_64.deb https://github.com/3proxy/3proxy/releases/download/0.9.3/3proxy-0.9.3.x86_64.deb
 dpkg -i /opt/3proxy-0.9.3.x86_64.deb && apt-get -f install
 
-
-# current directory
-CURRDIR=$(pwd)
-# 3proxy version
-PROXY_VER='0.9.1'
-
 # get server IP
 IP_GET_ITER=0
 echo '== Get external IP address ...'
@@ -28,27 +22,32 @@ done
 # set 3proxy config
 echo '== Set 3proxy config ...'
 cfg_file=/etc/3proxy/conf/3proxy.cfg
-cat >> ${cfg_file} << EOF
-#====#
+cat > ${cfg_file} << EOF
 daemon
-pidfile /tmp/3proxy.pid
-#config /etc/3proxy/3proxy.cfg
-internal ${IPV4_ADDR}
-external ${IPV4_ADDR}
+
+nscache 65536
 nserver 8.8.8.8
 nserver 8.8.4.4
-nscache 65536
-timeouts 1 5 30 60 180 1800 15 60
+
+config /conf/3proxy.cfg
+monitor /conf/3proxy.cfg
+
+internal ${IPV4_ADDR}
+external ${IPV4_ADDR}
+
 users \$/conf/passwd
-users fastusaproxycom:CL:Fastusaproxycom8080
-log /var/log/3proxy/3proxy.log H
+
+log /logs/3proxy.log D
 logformat "-,\''+_L%t,""%N"",%p,%E,""%U"",""%C"",%c,""%R"",%r,%O,%I,""%n"",""%T"" "
 rotate 1
+
+include /conf/counters
+include /conf/bandlimiters
 auth strong
 noforce
 allow *
-proxy -a -n -p45000 -e${IPV4_ADDR}
-socks -a -p46000 -e${IPV4_ADDR}
+proxy -a -n -p45000
+socks -a -p46000
 EOF
 
 # add crontab tasks
@@ -62,5 +61,5 @@ then
 fi
 
 # start proxy
-echo '== Running proxy ...'
-/etc/init.d/3proxy start
+#echo '== Running proxy ...'
+#/etc/init.d/3proxy start
